0c4777de4bf4b6cc1a1b33e360ab9be9d1da0f96
Seungyeon Han <tiffanyhansy@gmail.com>
2025-05-17 19:31:29 +0900

    feat: 디자인 시스템 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+const Colors = {
+  grayLightWhite: '#ffffff',
+  grayLight100: '#f6f8f9',
+  grayLight200: '#f2f4f6',
+  grayLight300: '#eaecee',
+  grayLight400: '#dcdfe3',
+  grayLight500: '#d1d4d8',
+  grayLight600: '#adb3ba',
+  grayLight700: '#868c94',
+  grayLight800: '#4d5359',
+  grayLight900: '#212224',
+  grayLightBlack: '#000000',
+
+  mainRed: '#FC5F46',
+  subGreen: '#BEE60A',
+  red100: '#FFE7E4E7',
+};
+
+export default Colors;
